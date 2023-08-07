68455d42ebc78832b9c5b8ca733662525a7f183b
Kim Love <kimberlymlove15@gmail.com>
2023-08-07 18:40:23 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName, userEmail){
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+        },
+        body: JSON.stringify({name: userName, email: userEmail})
+    })
+    .then(res => res.json())
+    .then(data => {
+        const p = document.createElement("p")
+        p.textContent = data.id
+        document.querySelector("body").append(p)
+    })
+    .catch(message =>{
+        const p = document.createElement("p")
+        p.textContent = message
+        document.querySelector("body").append(p)
+    })
+}
